9aff86d4c85501336b502771017571bab61d2202
Suvarna Ballid <suvarnaballid726@gmail.com>
2023-08-21 11:49:31 +0530

    Assignment
    Assignment

---
+++ Assignment-09_LabelledDoWhileLoop/src/Demo.java
@@ -0,0 +1,22 @@
+
+public class Demo 
+{
+public static void main(String[] args) 
+	{
+	int m=1;
+		suvarna:do
+		{
+			int n=1;
+			chinnu:do 
+			{
+				System.out.println("Kodnest");
+				n++;
+				break chinnu;
+			}while(n<=5);
+			System.out.println();
+			m++;
+		}while(m<=5);
+		System.out.println("Technologies");
+	}
+}
+
---
+++ Assignment-09_LabelledDoWhileLoop/src/Demo1.java
@@ -0,0 +1,24 @@
+
+public class Demo1
+{
+	public static void main(String[] args) 
+		{
+		int m=1;
+			vidya:do
+			{
+				int n=1;
+				divya:do 
+				{
+					System.out.println("Kodnest");
+					n++;
+					break divya;
+				}while(n<=5);
+				System.out.println();
+				m++;
+				break vidya;
+			}while(m<=5);
+			System.out.println("Technologies");
+		}
+	}
+
+
