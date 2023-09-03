108e695cb9c431352906d06e4d7d1cd5596ac9b2
Suvarna Ballid <suvarnaballid726@gmail.com>
2023-09-03 11:28:28 +0530

    Assignment about pattern programs
    Assignment_12
    pattern programs

---
+++ Assignment_12_PatternPrograms(A-Z)/src/K.java
@@ -0,0 +1,20 @@
+
+public class K {
+	public static void main(String[] args) {
+		int n=10;
+		for(int i=0;i<=n;i++)
+		{
+			for(int j=0;j<=n;j++)
+			{
+				if((i==j&&i>=n/2)||(j==n-i&&i<=n/2)||j==4){
+				
+					System.out.print("*");
+				}
+					else {
+					System.out.print(" ");
+			}
+			}
+				System.out.println();
+			}
+	}
+}
